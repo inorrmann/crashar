7ffe7fd33f5af0ce1e745a8a53bc7fab0e3f1918
Ingrid N <inorrmann@gmail.com>
2020-01-14 17:15:24 -0800

    home page finished

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,58 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { useAuth } from '../../utils/auth';
 import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Navbar";
-
-
-const createLink = ({ text, to, ...rest }) => {
-  const className = 'nav-link';
-  if (to) {
-    return (
-      <Link className={className} to={to} {...rest}>
-        {text}
-      </Link>
-    );
-  }
-  return (
-    <span
-      role="button"
-      className={className}
-      style={{ cursor: 'pointer' }}
-      {...rest}
-    >
-      {text}
-    </span>
-  );
-};
-
-function NavLinks() {
-  const { isLoggedIn, logout } = useAuth();
-  let links = [];
-  if (isLoggedIn) {
-    links.push({ text: 'Profile', to: '/profile' });
-    links.push({ text: 'Logout', onClick: () => logout() });
-  } else {
-    links.push({ text: 'Signup', to: '/signup' });
-    links.push({ text: 'Login', to: '/login' });
-  }
-  return (
-    <ul className="navbar-nav">
-      {links.map((link, i) => (
-        <li key={i} className="nav-item">
-          {createLink(link)}
-        </li>
-      ))}
-    </ul>
-  );
-}
 
 
 function NavBar(props) {
   return (
-    <Navbar style={{ fontFamily: "Roboto", fontSize: "1.2rem", color: "#EBC023"}}>
+    <Navbar style={{ fontFamily: "Roboto", fontSize: "1.2rem"}}>
       {props.children}
-      {/* <NavLinks  /> */}
     </Navbar>
   )
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,23 +3,20 @@ import Navbar from "../components/Navbar/Navbar";
 import Jumbotron from "../components/Jumbotron/index";
 import Nav from "react-bootstrap/Navbar";
 import NavLogin from "../components/NavbarLogin/index";
-
+import Button from "../components/ButtonLink/index"
 import "./style.css";
 
+const styleLogin = { color: "#EBC023" }
+const styleLink = { color: "#574F44" }
+const styleButton = { backgroundColor: "#EBC023", color: "#574F44", fontWeight: "bold" }
 
-// import NavbarLink from "../components/NavbarLink/index";
-// import Button from "../components/ButtonLink/index";
-
-const styleLink = { color: "#EBC023" }
-// const styleButton = { backgroundColor: "#EBC023", color: "#574F44", fontWeight: "bold" }
 
 function Home() {
     return (
         <div className="home">
             <Navbar>
                 <Nav className="ml-auto">
-                    <NavLogin style={styleLink} />
-                    {/* <NavLogin /> */}
+                    <NavLogin style={styleLogin} />
                 </Nav>
             </Navbar>
             <Jumbotron />
@@ -27,7 +24,7 @@ function Home() {
             <h6 className="text-center text-white px-5 homeText my-3">Have a National Park in mind to go camping, but all spots are already full?</h6>
             <h5 className="text-center text-white px-5 homeText">Find like-minded people to share a campsite with!</h5>
             <div className="text-center mt-5">
-                {/* <Button link="/signup" name="START NOW" style={styleButton} /> */}
+                <Button link="/signup" name="START NOW" style={styleButton} styleLink={styleLink}/>
             </div>
         </div>
     );
