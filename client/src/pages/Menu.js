7449993f163a2a72b599b420a2e29c8a9e83590b
Ingrid Norrmann-Vigil <inorrmann@gmail.com>
2020-04-01 16:34:35 -0700

    media query for main menu and loading screen completed

--- client/src/pages/Menu.js
+++ client/src/pages/Menu.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { useHistory } from 'react-router-dom';
 import { useAuth } from '../utils/auth';
 import Navbar from "../components/Navbar/Navbar";
 import NavLogin from "../components/NavbarLogin/index";
@@ -18,46 +17,46 @@ function Menu() {
 
   return (
     <div className="menu overflow-auto">
-      <div className="menuTop">
-
         <Navbar style={styleNavbar}>
           <NavLink link={`/messages/all/${user.id}`} styleLink={styleLink} name="Messages" />
           <div className="ml-auto">
             <NavLogin style={styleLogin} />
           </div>
         </Navbar>
-        <br></br>
-        <br></br>
-        <h1 className="display-4 text-center" style={{ fontWeight: "bold", color: "#EBC023", textShadow: "0 0 20px #0F0E0C" }}>Share</h1>
-        <div className="text-center">
-          <Button
-            style={styleButton}
-            link="/sites/new"
-            styleLink={styleButtonLink}
-            name="SHARE A CAMPSITE"
-          />
-        </div>
-        <div className="text-center my-2">
-          <Button
-            style={styleButton}
-            link={`/sites/all/${user.id}`}
-            styleLink={styleButtonLink}
-            name="SEE MY SHARED CAMPSITES"
-          />
+      <div className="menuTop">
+        <div className="shareButtons">
+          <h1 className="display-4 text-center" style={{ fontWeight: "bold", color: "#EBC023", textShadow: "0 0 20px #0F0E0C" }}>Share</h1>
+          <div className="menuButton text-center">
+            <Button
+              style={styleButton}
+              link="/sites/new"
+              styleLink={styleButtonLink}
+              name="SHARE A CAMPSITE"
+            />
+          </div>
+          <div className="menuButton text-center my-2">
+            <Button
+              style={styleButton}
+              link={`/sites/all/${user.id}`}
+              styleLink={styleButtonLink}
+              name="SEE MY SHARED CAMPSITES"
+            />
+          </div>
         </div>
       </div>
 
       <div className="menuBottom">
         <h4 className="text-center mt-3" style={{ fontFamily: "Barlow", fontWeight: "bold", color: "#0F0E0C", textShadow: "0 0 20px #FFF8D5" }}>Welcome {user.name}!</h4>
-        <br></br>
-        <h1 className="display-4 text-center" style={{ fontWeight: "bold", color: "#EBC023", textShadow: "0 0 12px rgba(15, 14, 12, 0.9)" }}>Crash</h1>
-        <div className="text-center">
-          <Button
-            style={styleButton}
-            link="/sites/search"
-            styleLink={styleButtonLink}
-            name="FIND OPEN CAMPSITES"
-          />
+        <div className="crashButton">
+          <h1 className="display-4 text-center" style={{ fontWeight: "bold", color: "#EBC023", textShadow: "0 0 12px rgba(15, 14, 12, 0.9)" }}>Crash</h1>
+          <div className="menuButton text-center">
+            <Button
+              style={styleButton}
+              link="/sites/search"
+              styleLink={styleButtonLink}
+              name="FIND OPEN CAMPSITES"
+            />
+          </div>
         </div>
       </div>
     </div>
